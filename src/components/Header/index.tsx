972d7d0d3b0c05053cd6e72819c7ba16129c56bc
Fillipe <fillipe484@gmail.com>
2021-12-01 18:56:56 -0300

    feat: finish test

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,17 @@
-export default function Header() {
-  // TODO
-}
+import Link from 'next/link';
+
+import styles from './header.module.scss';
+
+const Header: React.FC = () => {
+  return (
+    <header className={styles.header}>
+      <Link href="/">
+        <a>
+          <img src="/logo.svg" alt="logo" />
+        </a>
+      </Link>
+    </header>
+  );
+};
+
+export default Header;
